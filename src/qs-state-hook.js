e53851c6a5c23fc1e25bdd57e60de494883ec78d
Daniel da Silva <danielfdsilva@gmail.com>
2022-04-21 14:33:26 +0100

    Avoid using useRef for commit function

--- src/qs-state-hook.js
+++ src/qs-state-hook.js
@@ -31,36 +31,37 @@ let commitQueue = {};
 const identityFn = (v) => v;
 
 export function useQsStateCreator(options = {}) {
-  const {
-    commit: commitFn = history.push,
-    // location is a mutable object.
-    location = history.location
-  } = options;
+  const { commit: commitToLocation = history.push } = options;
 
-  const commitToLocation = useRef();
-  commitToLocation.current = commitFn;
+  // location is mutable. Use with useRef to prevent linting errors.
+  const location = useRef();
+  location.current = options.location || history.location;
 
-  return useMemo(() => {
-    const commit = debounce(() => {
-      const parsedQS = qs.parse(location.search, {
-        ignoreQueryPrefix: true
-      });
+  const commit = useMemo(
+    () =>
+      debounce(() => {
+        const parsedQS = qs.parse(location.current.search, {
+          ignoreQueryPrefix: true
+        });
 
-      // New object that is going to be stringified to the url.
-      // Current properties plus the ones to be changed.
-      const qsObject = {
-        ...parsedQS,
-        ...commitQueue
-      };
+        // New object that is going to be stringified to the url.
+        // Current properties plus the ones to be changed.
+        const qsObject = {
+          ...parsedQS,
+          ...commitQueue
+        };
 
-      commitToLocation.current({
-        search: qs.stringify(qsObject, { skipNulls: true })
-      });
-      // Once the commit happens clear the commit queue.
-      commitQueue = {};
-    }, COMMIT_DELAY);
+        commitToLocation({
+          search: qs.stringify(qsObject, { skipNulls: true })
+        });
+        // Once the commit happens clear the commit queue.
+        commitQueue = {};
+      }, COMMIT_DELAY),
+    [commitToLocation]
+  );
 
-    function storeInURL(k, v) {
+  const storeInURL = useCallback(
+    (k, v) => {
       // Store the new value in the queue.
       commitQueue = {
         ...commitQueue,
@@ -68,141 +69,151 @@ export function useQsStateCreator(options = {}) {
       };
       // Try to commit.
       commit();
-    }
+    },
+    [commit]
+  );
 
-    /**
-     * Qs State is used to sync a state object with url search string.
-     * It will keep the state value in sync with the url.
-     * The value will be validated according to the state definition
-     *
-     * Example:
-     * {
-     *    key: 'field',
-     *    hydrator: (v) => v,
-     *    dehydrator: (v) => v,
-     *    default: 'all',
-     *    validator: [1, 2, 3]
-     * }
-     *
-     * {string} key - Key name to use on the search string
-     * {func} hydrator - Any transformation to apply to the value from the search
-     *                   string before using it. Converting to number for example.
-     * {func} dehydrator - Any transformation to apply to the value before adding it
-     *                     to the search. Converting a number to string for example.
-     * {string|num} default - Default value. To note that when a state value is
-     *                        default it won't go to the search string.
-     * {array|func} validator - Validator function for the value. If is an array
-     *                          should contain valid options. If it is a function
-     *                          should return true or false.
-     *
-     * @param {object} definition The definition object.
-     *
-     */
-    function useQsState(def) {
-      const mounted = useRef(false);
-
-      // Setup defaults.
-      const {
-        // Function to convert the value from the string before using it.
-        hydrator = identityFn,
-        // Function to convert the value to the string before using it.
-        dehydrator = identityFn
-      } = def;
-
-      // Location search
-      const locSearch = location.search;
-
-      // Get the correct validator. Array of items, function or default.
-      const validator = useCallback(
-        (v) => {
-          const userValidator = def.validator;
-          if (userValidator && typeof userValidator.indexOf === 'function') {
-            return userValidator.indexOf(v) !== -1;
-          } else if (typeof userValidator === 'function') {
-            return userValidator(v);
-          } else {
-            return !!v;
-          }
-        },
-        [def.validator]
-      );
-
-      // Parse the value from the url.
-      const getValueFromURL = (searchString) => {
-        const parsedQS = qs.parse(searchString, { ignoreQueryPrefix: true });
-
-        // Hydrate the value:
-        // Convert from a string to the final type.
-        const value = hydrator(parsedQS[def.key]);
-
-        return validator(value) ? value : def.default;
-      };
-
-      // Store the state relative to this qs key.
-      const [valueState, setValueState] = useState(getValueFromURL(locSearch));
-
-      // We need a ref to store the state value, otherwise the closure created by
-      // the useEffect always shows the original value. We can't pass the value as
-      // a dependency of useEffect otherwise it would keep changing itself.
-      // Similar issue:
-      // https://stackoverflow.com/questions/57847594/react-hooks-accessing-up-to-date-state-from-within-a-callback
-      const stateRef = useRef();
-      stateRef.current = valueState;
-
-      const setValue = useCallback(
-        (value) => {
-          const v = validator(value) ? value : def.default;
-          // Dehydrate the value:
-          // Convert to a string usable in the url.
-          const dehydratedVal = dehydrator(v);
-
-          // Store value in state.
-          setValueState(v);
-
-          // Because defaults can be objects, we compare on the dehydrated value.
-          const dehydratedDefaultVal = dehydrator(def.default);
-          storeInURL(
-            def.key,
-            dehydratedVal !== dehydratedDefaultVal ? dehydratedVal : null
-          );
-        },
-        [validator, def.default, def.key, dehydrator]
-      );
-
-      // "Listen" to url changes and replace only if different from the currently
-      // stored state.
-      useEffect(() => {
-        // Ensure this check only runs once the component mounted.
-        // The initial url setting is done when the state is initialized.
-        if (!mounted.current) {
-          mounted.current = true;
-          return;
-        }
-
-        const v = getValueFromURL(locSearch);
-
-        // The url should only be checked as a source of truth once all the
-        // commits have settled.
-        const hasPendingCommits = Object.keys(commitQueue).length;
-
-        if (!hasPendingCommits && !isEqualObj(v, stateRef.current)) {
-          setValueState(v);
-        }
-        /* eslint-disable-next-line react-hooks/exhaustive-deps */
-      }, [locSearch, def]);
-
-      return [valueState, setValue];
-    }
-
-    // This version is just a way to make the use with memo quicker.
-    useQsState.memo = function useQsStateMemoized(v, deps = []) {
-      /* eslint-disable-next-line react-hooks/exhaustive-deps */
-      return useQsState(useMemo(() => v, deps));
-    };
-
-    return useQsState;
-    /* eslint-disable-next-line react-hooks/exhaustive-deps */
-  }, []);
+  return useMemo(
+    () => qsStateFactory({ storeInURL, location: location.current }),
+    [storeInURL]
+  );
 }
 
 export default useQsStateCreator;
+
+function qsStateFactory({ storeInURL, location }) {
+  /**
+   * Qs State is used to sync a state object with url search string.
+   * It will keep the state value in sync with the url.
+   * The value will be validated according to the state definition
+   *
+   * Example:
+   * {
+   *    key: 'field',
+   *    hydrator: (v) => v,
+   *    dehydrator: (v) => v,
+   *    default: 'all',
+   *    validator: [1, 2, 3]
+   * }
+   *
+   * {string} key - Key name to use on the search string
+   * {func} hydrator - Any transformation to apply to the value from the search
+   *                   string before using it. Converting to number for example.
+   * {func} dehydrator - Any transformation to apply to the value before adding it
+   *                     to the search. Converting a number to string for example.
+   * {string|num} default - Default value. To note that when a state value is
+   *                        default it won't go to the search string.
+   * {array|func} validator - Validator function for the value. If is an array
+   *                          should contain valid options. If it is a function
+   *                          should return true or false.
+   *
+   * @param {object} definition The definition object.
+   *
+   */
+  function useQsState(def) {
+    const mounted = useRef(false);
+
+    // Setup defaults.
+    const {
+      // Function to convert the value from the string before using it.
+      hydrator = identityFn,
+      // Function to convert the value to the string before using it.
+      dehydrator = identityFn
+    } = def;
+
+    // Location search
+    const locSearch = location.search;
+
+    // Get the correct validator. Array of items, function or default.
+    const validator = useCallback(
+      (v) => {
+        const userValidator = def.validator;
+        if (userValidator && typeof userValidator.indexOf === 'function') {
+          return userValidator.indexOf(v) !== -1;
+        } else if (typeof userValidator === 'function') {
+          return userValidator(v);
+        } else {
+          return !!v;
+        }
+      },
+      [def.validator]
+    );
+
+    // Parse the value from the url.
+    const getValueFromURL = (searchString) => {
+      const parsedQS = qs.parse(searchString, { ignoreQueryPrefix: true });
+
+      // Hydrate the value:
+      // Convert from a string to the final type.
+      const value = hydrator(parsedQS[def.key]);
+
+      return validator(value) ? value : def.default;
+    };
+
+    // Store the state relative to this qs key.
+    const [valueState, setValueState] = useState(getValueFromURL(locSearch));
+
+    // We need a ref to store the state value, otherwise the closure created by
+    // the useEffect always shows the original value. We can't pass the value as
+    // a dependency of useEffect otherwise it would keep changing itself.
+    // Similar issue:
+    // https://stackoverflow.com/questions/57847594/react-hooks-accessing-up-to-date-state-from-within-a-callback
+    const stateRef = useRef();
+    stateRef.current = valueState;
+
+    const setValue = useCallback(
+      (value) => {
+        const v = validator(value) ? value : def.default;
+        // Dehydrate the value:
+        // Convert to a string usable in the url.
+        const dehydratedVal = dehydrator(v);
+
+        // Store value in state.
+        setValueState(v);
+
+        // Because defaults can be objects, we compare on the dehydrated value.
+        const dehydratedDefaultVal = dehydrator(def.default);
+        storeInURL(
+          def.key,
+          dehydratedVal !== dehydratedDefaultVal ? dehydratedVal : null
+        );
+      },
+      // storeInURL is a necessary dependency despite being outer scope. If the
+      // storeInURL value changes then useQsState is going to run again which
+      // means that this callback will be recreated.
+      [validator, def.default, def.key, dehydrator, storeInURL]
+    );
+
+    // "Listen" to url changes and replace only if different from the currently
+    // stored state.
+    useEffect(() => {
+      // Ensure this check only runs once the component mounted.
+      // The initial url setting is done when the state is initialized.
+      if (!mounted.current) {
+        mounted.current = true;
+        return;
+      }
+
+      const v = getValueFromURL(locSearch);
+
+      // The url should only be checked as a source of truth once all the
+      // commits have settled.
+      const hasPendingCommits = Object.keys(commitQueue).length;
+
+      if (!hasPendingCommits && !isEqualObj(v, stateRef.current)) {
+        setValueState(v);
+      }
+      /* eslint-disable-next-line react-hooks/exhaustive-deps */
+    }, [locSearch, def]);
+
+    return [valueState, setValue];
+  }
+
+  // This version is just a way to make the use with memo quicker.
+  useQsState.memo = function useQsStateMemoized(v, deps = []) {
+    /* eslint-disable-next-line react-hooks/exhaustive-deps */
+    return useQsState(useMemo(() => v, [...deps, storeInURL]));
+  };
+
+  return useQsState;
+}
